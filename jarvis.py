5fce5cacfa3dfd9c7d292d64dbda7361b64f1bed
mdiallo98 <mamadou1237@gmail.com>
2022-07-07 14:59:46 -0400

    add a gui that allows dynamic user input

--- jarvis.py
+++ jarvis.py
@@ -1,8 +1,25 @@
 import wolframalpha
-
+import PySimpleGUI as sg
 app_id = '7PU8K2-TYL4XK4G5W'
 client = wolframalpha.Client(app_id)
 
-res = client.query('temperature in new york city today')
 
-print(next(res.results).text)
+# ==========================gui code===========================================================
+sg.theme('Black')   # Add a t ouch of color
+# All the stuff inside your window.
+layout = [[sg.Text('How May I Help', background_color='Black', text_color='Green')],
+          [sg.Text('Ask Away')],
+          [sg.InputText()],
+          [sg.Button('Ok'), sg.Button('Cancel')]]
+
+# Create the Window
+window = sg.Window('Window Title', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        break
+    res = client.query(values[0])
+    print(next(res.results).text)
+
+window.close()
