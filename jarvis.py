e8fbc3cc20967c9f71285fa1ed212caf0e67bd4a
mdiallo98 <mamadou1237@gmail.com>
2022-07-07 17:25:39 -0400

    included text to speech

--- jarvis.py
+++ jarvis.py
@@ -1,10 +1,12 @@
+
 import wolframalpha
 import PySimpleGUI as sg
 import wikipedia
+import pyttsx3
+
 
 app_id = '7PU8K2-TYL4XK4G5W'
 client = wolframalpha.Client(app_id)
-
 
 # ==========================gui code===========================================================
 sg.theme('Black')   # Add a t ouch of color
@@ -22,10 +24,15 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
 
+    engine = pyttsx3.init()
     wiki_res = wikipedia.summary(values[0], sentences=2)
     res = client.query(values[0])
     wolframa_res = next(res.results).text
-    sg.Popup("WolframAlpha: " + wolframa_res, "WikiPedia: " + wiki_res)
-
+    engine = pyttsx3.init()
+    engine.say(wolframa_res)
+    engine.say(wiki_res)
+    sg.PopupNonBlocking("WolframAlpha: " + wolframa_res,
+                        "WikiPedia: " + wiki_res)
+    engine.runAndWait()
 
 window.close()
