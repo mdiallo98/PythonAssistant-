5f0ebdade22a28d1515bd02716ae569acd551426
mdiallo98 <mamadou1237@gmail.com>
2022-07-07 14:37:29 -0400

    incorparated wolframa alpha

--- jarvis.py
+++ jarvis.py
@@ -1,2 +1,8 @@
-app_id = getfixture('7PU8K2-TYL4XK4G5W')
-client = Client(app_id)
+import wolframalpha
+
+app_id = '7PU8K2-TYL4XK4G5W'
+client = wolframalpha.Client(app_id)
+
+res = client.query('temperature in new york city today')
+
+print(next(res.results).text)
