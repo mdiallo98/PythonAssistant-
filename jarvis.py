225bca8afa680af855c925380b10b74a2675edd5
mdiallo98 <mamadou1237@gmail.com>
2022-07-07 16:20:57 -0400

    merged both wolframa and wikipedia

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,7 @@
 import wolframalpha
 import PySimpleGUI as sg
+import wikipedia
+
 app_id = '7PU8K2-TYL4XK4G5W'
 client = wolframalpha.Client(app_id)
 
@@ -17,10 +19,13 @@ window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel':
         break
+
+    wiki_res = wikipedia.summary(values[0], sentences=2)
     res = client.query(values[0])
-    sg.Popup(next(res.results).text)
+    wolframa_res = next(res.results).text
+    sg.Popup("WolframAlpha: " + wolframa_res, "WikiPedia: " + wiki_res)
 
 
 window.close()
